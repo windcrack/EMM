ca482a0e49aa393ffd3378f7e170c9729c57c30e
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2023-10-13 16:18:58 +0300

    add select2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ import { slider } from "./module/slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const isMobile = window.matchMedia("(max-width: 800px)").matches;
+  $(".js-select").select2({
+    minimumResultsForSearch: Infinity,
+    //dropdownCssClass: "form-select__wrap",
+  });
+
   slider();
   if (isMobile) {
     toggleMenuMob();
